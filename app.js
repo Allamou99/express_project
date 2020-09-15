e9156b6ff1a65b6d30dd52444b792bcb11e3c91c
Soufiane Allamou <soufianeallamou99@outlook.fr>
2020-09-15 14:55:43 +0100

    sessions part 2 login and signup

--- app.js
+++ app.js
@@ -39,51 +39,31 @@ app.use(session({
   resave:false,
   store: new FileStore()
 }))
-function auth(req,res,next){
-  console.log(req.signedCookies);
-  if(!req.session.user){
-    var authHeader = req.headers.authorization;
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-    if(!authHeader){
-      var err = new Error('You are not authenticated');
-      res.setHeader('WWW-Authenticate','Basic');
-      err.status = 401;
+function auth (req, res, next) {
+    console.log(req.session);
+
+  if(!req.session.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
       return next(err);
-    }
-
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var username = auth[0];
-    var password = auth[1];
-    
-    if(username == 'admin' && password == 'password'){
-      req.session.user = 'admin';
+  }
+  else {
+    if (req.session.user === 'authenticated') {
       next();
     }
-    else{
-      var err = new Error('You are not authenticated');
-      res.setHeader('WWW-Authenticate','Basic');
-      err.status = 401;
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
       return next(err);
     }
   }
-  else{
-    if(req.session.user === 'admin'){
-      next();
-    }
-    else{
-      var err = new Error('You are not authenticated');
-      res.setHeader('WWW-Authenticate','Basic');
-      err.status = 401;
-      return next(err);
-    }
-  }
-  
 }
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
